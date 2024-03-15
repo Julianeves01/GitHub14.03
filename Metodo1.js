7faef40a3dd22c3f58b9b9045d6542e1aaa9a644
Juliasneves01 <julianvs2007@gmail.com>
2024-03-15 14:00:42 -0300

    Array

---
+++ Metodo1.js
@@ -0,0 +1,12 @@
+//Array.projetotype.pop()
+//O método pop remove o último elemento de um array
+
+//Exemplo1:
+nomes = ["ana clara", "flavia", "sophia", "amanda"];
+nomes.pop();
+console.log(nomes);
+
+//Exemplo2: 
+sobrenomes = ["silva", "castro", "santos", "ribeiro"];
+sobrenomes.pop();
+console.log(sobrenomes);
