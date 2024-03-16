9fd9df01fa60529f4c1aca6bf27fdcb45e932c38
Juliasneves01 <julianvs2007@gmail.com>
2024-03-16 10:46:40 -0300

    Adicionei 2 exemplos do método shift

---
+++ Metodo5.js
@@ -0,0 +1,16 @@
+//Array.prototype.shift()
+//O método shift() remove o primeiro elemento de um array e retorna esse elemento.
+
+//Exemplo1:
+materialEscolar = ["caderno", "lápis", "caneta"];
+
+const firstElement = materialEscolar.shift();
+
+console.log(materialEscolar);
+
+//Exemplo2:
+carros = ["Lamborghini", "Ferrari", "BMW"];
+
+const Firstelement = carros.shift();
+
+console.log(carros);
