cf37133d55eb826cb40a9f631b86f3f824c57b7e
Juliasneves01 <julianvs2007@gmail.com>
2024-03-16 14:19:54 -0300

    Adicionei 2 exemplos do método concat

---
+++ Metodo12.js
@@ -0,0 +1,20 @@
+//Array.prototype.concat()
+//O método concat() é utilizado para mesclar dois ou mais arrays.
+
+//Exemplo1:
+elogios1= ["linda", "perfeita", "maravilhosa"];
+elogios2 = ["bonita", "encantadora"];
+elogios3 = elogios1.concat(elogios2);
+
+console.log(elogios3);
+
+//Resultado esperado: ['linda', 'perfeita', 'maravilhosa', 'bonita', 'encantadora']
+
+//Exemplo2: 
+
+closet1 = ["roupas", "calçados"];
+closet2 = ["acessórios", "perfumes"]
+closet3 = closet1.concat(closet2);
+
+console.log(closet3);
+
