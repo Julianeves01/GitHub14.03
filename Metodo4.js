c8b268e1553ba979f58cfd03bfb7916461b5b9e3
Juliasneves01 <julianvs2007@gmail.com>
2024-03-16 10:36:03 -0300

    Adicionei 2 exemplos do método reverse

---
+++ Metodo4.js
@@ -0,0 +1,12 @@
+//Array.prototype.reverse()
+//O método reverse() inverte os itens de um array. O primeiro elemento do array se torna o último e o último torna-se o primeiro.
+
+//Exemplo1:
+objetos = ["oculos", "anel", "colar"];
+objetos.reverse();
+console.log(objetos);
+
+//Exemplo2: 
+calçados = ["tênis", "salto", "sandália"];
+calçados.reverse();
+console.log(calçados);
