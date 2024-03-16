a7107ae210d1d4e572f6c5047727b6fdc43c99dd
Juliasneves01 <julianvs2007@gmail.com>
2024-03-16 11:00:35 -0300

    Adicionei 2 exemplos do método slice

---
+++ Metodo6.js
@@ -0,0 +1,12 @@
+//Array.prototype.slice()
+//O método slice() retorna uma cópia de parte de um array a partir de um subarray criado entre as posições início e fim (fim não é incluído) de um array original. O Array original não é modificado.
+
+//Exemplo1:
+esportes= ["Atletismo", "Futebol", "Corrida", "Basquete", "Vôlei"];
+
+console.log(esportes.slice(2));
+
+//Exemplo2:
+times = ["Corinthians", "Palmeiras", "Flamengo ", "São Paulo", "Fluminense"];
+
+console.log(times.slice(0));
